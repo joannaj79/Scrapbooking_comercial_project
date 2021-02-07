ce486e1f547e079b947316978c944c0590ca981f
Joanna Jarzynska <jarzynskaj@gmail.com>
2021-02-07 18:08:00 +0100

    Scrapbook project updates

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
-var test = document.getElementById("borderLine");
+new WOW().init();
+
+const firstLine = () => {
+  const test = document.getElementById("borderLine");
+  let windowTop = window.pageYOffset - 500;
+  let testTop = test.getBoundingClientRect().top;
+  if (windowTop > testTop) {
+    test.style.animation = "mymove 3s linear";
+    test.style.display = "block";
+  }
+};
+
+const secondLine = () => {
+  const testTwo = document.getElementById("borderLineTwo");
+  let windowTop = window.pageYOffset - 1500;
+  let testTwoTop = testTwo.getBoundingClientRect().top;
+  if (windowTop > testTwoTop) {
+    testTwo.style.animation = "mymove 3s linear";
+    testTwo.style.display = "block";
+  }
+};
+const thirdLine = () => {
+  const testThree = document.getElementById("borderLineThree");
+  let windowTop = window.pageYOffset - 2000;
+  let testThreeTop = testThree.getBoundingClientRect().top;
+  console.log(testThreeTop);
+  if (windowTop > testThreeTop) {
+    testThree.style.animation = "mymove 3s linear";
+    testThree.style.display = "block";
+  }
+};
 
 window.onscroll = function () {
-  if (document.documentElement.scrollTop > test.getBoundingClientRect().top) {
-    test.style.animation = "mymove 3s linear";
-  }
+  firstLine();
+  secondLine();
+  thirdLine();
 };
-window.onscroll = function () {
-  if (document.documentElement.scrollTop > test.getBoundingClientRect().top) {
-    test.style.animation = "mymove 3s linear";
-  }
-};
+
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".navigationContainer");
+    $(".navigationLinksWrap").toggleClass(
+      "scrolled",
+      $(this).scrollTop() > $nav.height()
+    );
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
   });
 });
-new WOW().init();
